a1f178ff0b4b8bb01b04fb70288ac2311ce12e2b
Chad Upjohn <chad.upjohn@pinnsg.com>
2024-04-21 16:17:51 -0500

    Updating with formatting

--- invoice_processing.py
+++ invoice_processing.py
@@ -4,16 +4,25 @@ import pyspark.sql.functions as F
 
 # COMMAND ----------
 
-@dlt.table()
-@dlt.expect_all_or_drop({'customer_id_not_null': 'customer_id is not NULL', 'customer_name_more_than_one_char': 'len(customer_name) > 1'})
-def customer_bronze():
-    return dlt.read_stream('customer_raw').select(F.col('CustomerID').cast('int').alias('customer_id'), F.col('CustomerName').alias('customer_name'))
 
+@dlt.table()
+@dlt.expect_all_or_drop(
+    {
+        "customer_id_not_null": "customer_id is not NULL",
+        "customer_name_more_than_one_char": "len(customer_name) > 1",
+    }
+)
+def customer_bronze():
+    return dlt.read_stream("customer_raw").select(
+        F.col("CustomerID").cast("int").alias("customer_id"),
+        F.col("CustomerName").alias("customer_name"),
+    )
 
 
 # COMMAND ----------
 
+
 @dlt.table
-@dlt.expect_all_or_drop({'invoice_no_not_null': 'InvoiceNo is not NULL'})
+@dlt.expect_all_or_drop({"invoice_no_not_null": "InvoiceNo is not NULL"})
 def invoices_bronze():
-    return dlt.read_stream('invoices_raw')
+    return dlt.read_stream("invoices_raw")
